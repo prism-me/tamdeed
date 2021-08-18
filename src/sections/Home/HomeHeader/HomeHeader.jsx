ba531b3d883acd77bb60dfd7a04466679a5560c5
faiza-zakir <faiza.prism@gmail.com>
2021-08-18 09:41:11 +0500

    backgrounds waves

--- src/sections/Home/HomeHeader/HomeHeader.jsx
+++ src/sections/Home/HomeHeader/HomeHeader.jsx
@@ -9,52 +9,79 @@ import ModalVideo from 'react-modal-video'
 function HomeHeader(props) {
   const [isOpen, setOpen] = useState(false)
   return (
-    <div
-      className="home-header wave-container"
-      style={{ backgroundImage: `url(${header_bg})` }}
-    >
-      <Container>
-        <Row>
-          <Col sm={5}>
-            <div className="description-column">
-              <div className="header-logo-wrapper">
-                <img src={logo} alt="AGS Logo" className="header-logo" />
-              </div>
-              <h1 className="intro-title">
-                AMERICAN GULF SCHOOL
-              </h1>
-              <h2 className={"intro-title2"}>
-                EDUCATION DONE RIGHT
-              </h2>
-              <p className={"subtitle"}>
-                Take a look at what the CEO of the school
-                <br /> has to say about.
-              </p>
-            </div>
-          </Col>
-          <Col sm={{ span: 6}} className={"iconSpace"}>
-            <div className="video-promo-content">
-              <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="RBZutrFFhTA" onClose={() => setOpen(false)} />
-              <button className="btn About-video-play-icon" onClick={()=> setOpen(true)}>
-                <PlayArrowIcon className={"AboutVideoplaySize"}/>
-              </button>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      {/*<svg viewBox="0 0 500 500"*/}
-      {/*     preserveAspectRatio="xMinYMin meet"*/}
-      {/*     style={{zIndex:"-3"}}>*/}
-
-      {/*  <path d="M0, 100 C150, 300 350,*/}
-      {/*          0 500, 100 L500, 00 L0, 0 Z"*/}
-      {/*        style={{*/}
-      {/*          stroke: "none",*/}
-      {/*          fill: "rgba(220, 20, 60, 0.5)"*/}
-      {/*        }}>*/}
-      {/*  </path>*/}
-      {/*</svg>*/}
-    </div>
+      <>
+        <div className="home-header"
+             style={{ backgroundImage: `url(${header_bg})` }}
+        >
+          <div className={"inner-header flex"}>
+            <Container>
+              <Row>
+                <Col sm={5}>
+                  <div className="description-column">
+                    <div className="header-logo-wrapper">
+                      <img src={logo} alt="AGS Logo" className="header-logo" />
+                    </div>
+                    <h1 className="intro-title">
+                      AMERICAN GULF SCHOOL
+                    </h1>
+                    <h2 className={"intro-title2"}>
+                      EDUCATION DONE RIGHT
+                    </h2>
+                    <p className={"subtitle"}>
+                      Take a look at what the CEO of the school
+                      <br /> has to say about.
+                    </p>
+                  </div>
+                </Col>
+                <Col sm={{ span: 6}} className={"iconSpace"}>
+                  <div className="video-promo-content">
+                    <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="RBZutrFFhTA" onClose={() => setOpen(false)} />
+                    <button className="btn About-video-play-icon" onClick={()=> setOpen(true)}>
+                      <PlayArrowIcon className={"AboutVideoplaySize"}/>
+                    </button>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+          <div className={"waves"}>
+          </div>
+        </div>
+    {/*<div*/}
+    {/*  className="home-header"*/}
+    {/*  style={{ backgroundImage: `url(${header_bg})` }}*/}
+    {/*>*/}
+    {/*  <Container>*/}
+    {/*    <Row>*/}
+    {/*      <Col sm={5}>*/}
+    {/*        <div className="description-column">*/}
+    {/*          <div className="header-logo-wrapper">*/}
+    {/*            <img src={logo} alt="AGS Logo" className="header-logo" />*/}
+    {/*          </div>*/}
+    {/*          <h1 className="intro-title">*/}
+    {/*            AMERICAN GULF SCHOOL*/}
+    {/*          </h1>*/}
+    {/*          <h2 className={"intro-title2"}>*/}
+    {/*            EDUCATION DONE RIGHT*/}
+    {/*          </h2>*/}
+    {/*          <p className={"subtitle"}>*/}
+    {/*            Take a look at what the CEO of the school*/}
+    {/*            <br /> has to say about.*/}
+    {/*          </p>*/}
+    {/*        </div>*/}
+    {/*      </Col>*/}
+    {/*      <Col sm={{ span: 6}} className={"iconSpace"}>*/}
+    {/*        <div className="video-promo-content">*/}
+    {/*          <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="RBZutrFFhTA" onClose={() => setOpen(false)} />*/}
+    {/*          <button className="btn About-video-play-icon" onClick={()=> setOpen(true)}>*/}
+    {/*            <PlayArrowIcon className={"AboutVideoplaySize"}/>*/}
+    {/*          </button>*/}
+    {/*        </div>*/}
+    {/*      </Col>*/}
+    {/*    </Row>*/}
+    {/*  </Container>*/}
+    {/*</div>*/}
+        </>
   );
 }
 

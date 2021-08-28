238f9c843eeff33fc5dc89ceb6c746ea006d2eb3
himanshu kanpariya <patevinod84@gmail.com>
2021-08-28 15:46:05 +0530

    Primaryschool page and Careers page integration

--- src/pages/Primaryschool.jsx
+++ src/pages/Primaryschool.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Schoolinclusion from "../sections/Primaryschool/Inclusion/Schoolinclusion";
 import Contactpannel from "../sections/Contactpannel";
 
-
 export default class Primaryschool extends Component {
 
   render() {

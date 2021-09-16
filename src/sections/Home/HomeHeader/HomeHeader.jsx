79b7f4b7ad7d48a2e23b1b3b6299dc4a903fb404
faiza-zakir <faiza.prism@gmail.com>
2021-09-16 16:50:40 +0500

    updated Files

--- src/sections/Home/HomeHeader/HomeHeader.jsx
+++ src/sections/Home/HomeHeader/HomeHeader.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import logo from "./../../../assets/images/agslogo/Logo (1).png";
-import header_bgMB from "./../../../assets/images/agsbanners/HomePage.jpg";
+import header_bgMB from "./../../../assets/images/agsbanners/HomePage.JPG";
 import header_bg from "./../../../assets/images/agsbanners/homepageBanner.png";
 import AGSPrincipalWelcomeMessage from "./../../../assets/images/bannerVideo/AGSPrincipalWelcomeMessage.mp4";
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';

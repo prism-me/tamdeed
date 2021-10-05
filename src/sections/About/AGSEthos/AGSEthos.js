ef7bc5a9b591b6262dc5b571cca6e1af79e58244
faiza-zakir <faiza.prism@gmail.com>
2021-10-05 11:40:06 +0500

    image changes

--- src/sections/About/AGSEthos/AGSEthos.js
+++ src/sections/About/AGSEthos/AGSEthos.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from "@material-ui/core";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import bg_img_e from "../../../assets/images/agsbackgrounds/AGSEthosPhoto.jpg"
+import bg_img_e from "../../../assets/images/agsbackgrounds/AGSEthos.jpg"
 import { Hidden } from "@material-ui/core";
 
 const AgsEthos = () => {

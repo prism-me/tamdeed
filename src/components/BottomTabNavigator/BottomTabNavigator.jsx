18fc2d5a291c57c385be3a6ad253763aa4810215
faiza-zakir <faiza.prism@gmail.com>
2022-01-06 18:41:41 +0500

    change bottomnavigator icon

--- src/components/BottomTabNavigator/BottomTabNavigator.jsx
+++ src/components/BottomTabNavigator/BottomTabNavigator.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import { Link } from "react-router-dom";
-import ListIcon from '@material-ui/icons/List';
 import SearchIcon from '@material-ui/icons/Search';
 import PhoneIcon from '@material-ui/icons/Phone';
 import HomeIcon from '@material-ui/icons/Home';
+import CategoryIcon from '@material-ui/icons/Category';
 
 const useStyles = makeStyles({
   root: {
@@ -16,7 +16,6 @@ const useStyles = makeStyles({
 function BottomTabNavigator(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState("");
-  const [modalShow, setModalShow] = React.useState(false);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -40,7 +39,7 @@ function BottomTabNavigator(props) {
         </Link>
         <Link to={`#`}>
           <div className={"d-flex flex-column justify-content-center align-items-center"}>
-            <ListIcon className={"iconSize"} />
+            <CategoryIcon className={"iconSize"} />
             <p className={"textSize"}>
               Categories
             </p>

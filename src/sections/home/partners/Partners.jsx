c53c2b7dde11c7f30eebaa8a801e2265e8f57dbd
adnan bashir <adnanbashir3358@gmail.com>
2022-01-19 16:16:22 +0400

    partners extended

--- src/sections/home/partners/Partners.jsx
+++ src/sections/home/partners/Partners.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import partner1 from "../../../assets/images/partners/partner1.png"
 import partner2 from "../../../assets/images/partners/partner2.png"
@@ -8,9 +8,19 @@ import partner5 from "../../../assets/images/partners/partner5.png"
 import partner6 from "../../../assets/images/partners/partner6.png"
 import partner7 from "../../../assets/images/partners/partner7.png"
 import partner8 from "../../../assets/images/partners/partner8.png"
+import partner9 from "../../../assets/images/partners/partner9.png"
+import partner10 from "../../../assets/images/partners/partner10.png"
+import partner11 from "../../../assets/images/partners/partner11.png"
+import partner12 from "../../../assets/images/partners/partner12.png"
+import partner13 from "../../../assets/images/partners/partner13.png"
+import partner14 from "../../../assets/images/partners/partner14.png"
+import partner15 from "../../../assets/images/partners/partner15.png"
+import partner16 from "../../../assets/images/partners/partner16.png"
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 export default function Partners() {
+    const [showAllPartners, setShowAllPartners] = useState(false);
+    
     const slidesData = [
         {
             icon: partner1
@@ -37,6 +47,33 @@ export default function Partners() {
             icon: partner8
         }
     ];
+    const slidesData2 = [
+        {
+            icon: partner9
+        },
+        {
+            icon: partner10
+        },
+        {
+            icon: partner11
+        },
+        {
+            icon: partner12
+        },
+        {
+            icon: partner13
+        },
+        {
+            icon: partner14
+        },
+        {
+            icon: partner15
+        },
+        {
+            icon: partner16
+        }
+    ];
+
     return (
         <div className={"Partners"}>
             <Container>
@@ -56,8 +93,26 @@ export default function Partners() {
                         </Row>
                     </Col>
                 </Row>
+                {showAllPartners &&
+                <Row>
+                    <Col sm={2} className={"d-flex justify-content-center align-items-center"}>
+                        <h3 className={"Title"}></h3>
+                    </Col>
+                    <Col sm>
+                        <Row className={"mb-3"}>
+                            {
+                                slidesData2.map((slides, index) => (
+                                    <Col sm={3} xs={6} md={3} lg={3} className={"colmblspace partnerLogoDiv"} key={index}>
+                                        <img src={slides.icon} alt="brand" className={"img-fluid mblImg partnerLogoImg"} />
+                                    </Col>
+                                ))
+                            }
+                        </Row>
+                    </Col>
+                </Row>
+                }
                 <div className="d-flex justify-content-center align-items-center">
-                    <button className="btnStyle">See All <ChevronRightIcon /></button>
+                    <button className="btnStyle" onClick={() => setShowAllPartners(!showAllPartners)}> {showAllPartners ? "See Less" : "See All"} <ChevronRightIcon /></button>
                 </div>
             </Container>
         </div>

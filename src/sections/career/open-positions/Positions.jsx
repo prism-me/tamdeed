ecb1b0638f23c29098e4c1af4b1ebad2a4fa5e72
Payal Bhisikar <devteam2@prism-me.com>
2022-02-15 12:20:02 +0400

    career page job section done

--- src/sections/career/open-positions/Positions.jsx
+++ src/sections/career/open-positions/Positions.jsx
@@ -1,26 +1,72 @@
-import React from "react";
-import { Container } from "react-bootstrap"
+import React, { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap"
+import Designations from "../../../assets/images/icons/Color/Designations Icon.png"
 import icon from "../../../assets/images/icons/Color/Mask Group-4.png"
+import { API } from "../../../http/API"
 
 export default function Positions() {
+
+    const [allJobs, setAllJobs] = useState([]);
+    const [visible, setVisible] = useState(3);
+
+    useEffect(() => {
+        getAllJobs()
+    }, [])
+
+    const getAllJobs = () => {
+        API.get(`/jobs`, {
+        }).then((response) => {
+            setAllJobs(response.data.data)
+            console.log("jobs :: ", response.data.data)
+        });
+    }
+
+    const loadMore = () => {
+        setVisible(visible + 3);
+    }
 
     return (
         <div className={"positions"}>
             <h3 className={"title"}>
                 Open Positions
             </h3>
-            <Container>
-                <div className="positionDiv">
-                    <div className={"d-flex justify-content-center align-items-center"}>
-                        <span className={"iconBg"}>
-                            <img src={icon} alt="solution" className={"iconImg"} />
-                        </span>
+            {allJobs?.length === 0 ?
+                <Container>
+                    <div className="positionDiv">
+                        <div className={"d-flex justify-content-center align-items-center"}>
+                            <span className={"iconBg"}>
+                                <img src={icon} alt="solution" className={"iconImg"} />
+                            </span>
+                        </div>
+                        <p className={"subtitle"}>
+                            Coming Soon
+                        </p>
                     </div>
-                    <p className={"subtitle"}>
-                        Coming Soon
-                    </p>
+                </Container> :
+                <div className="AboutPartners allJobs">
+                    <Container>
+                        <Row>
+                            {allJobs?.slice(0, visible).map((item, index) => (
+                                <Col sm="4" key={index}>
+                                    <div className="positionDiv">
+                                        <div className={"d-flex justify-content-center align-items-center"}>
+                                            <span className={"iconBg"}>
+                                                <img src={Designations} alt="solution" className={"iconImg"} />
+                                            </span>
+                                        </div>
+                                        <p className={"subtitle"}>{item.designation}</p>
+                                        <p className={"job_description"}>{item.decription}</p>
+                                        <a href="mailto:info@tamdeed.ae" >ApplyNow</a>
+                                    </div>
+                                </Col>
+                            ))}
+                            {visible < allJobs?.length &&
+                                <button onClick={loadMore} type="button" className="load-more">Load more</button>
+                            }
+                        </Row>
+                    </Container>
                 </div>
-            </Container>
+            }
         </div>
     )
 }
